88630ba8c5886fb13ce747e0248d50c89fcbfdd6
SANDIPpandey1 <102663858+SANDIPpandey1@users.noreply.github.com>
2024-05-10 21:52:05 +0545

    Control System syllabus added

--- lib/screens/components/insidecard.dart
+++ lib/screens/components/insidecard.dart
@@ -9,6 +9,7 @@ import 'package:ioe/Subject_Pages/Computer%20Organization%20And%20Architecture.d
 import 'package:ioe/Subject_Pages/Computer%20networt%20and%20Security.dart';
 import 'package:ioe/Subject_Pages/Concrete%20Technology%20and%20Masonry%20Structure.dart';
 import 'package:ioe/Subject_Pages/Construction%20Management.dart';
+import 'package:ioe/Subject_Pages/Control%20System.dart';
 import 'package:ioe/Subject_Pages/Data%20Communication.dart';
 import 'package:ioe/Subject_Pages/Database%20Management%20System.dart';
 import 'package:ioe/Subject_Pages/Design%20of%20RCC%20Structure.dart';
@@ -716,6 +717,13 @@ class InsideCard extends StatelessWidget {
           MaterialPageRoute(
               builder: (context) =>
                   ELECTRICCIRCUITSANDMACHINES(initialTabIndex: 1)),
+        );
+        break;
+      case 'Control System':
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ControlSystem(initialTabIndex: 1)),
         );
         break;
       // Add cases for other subjects and their corresponding pages
